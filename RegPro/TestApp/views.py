6385e8d7d4b1170b3d277ded97f9457f59a9b406
Harisundarreddy <121248428+Harisundarreddy@users.noreply.github.com>
2024-07-30 13:15:56 +0530

    Delete RegPro/TestApp directory

--- RegPro/TestApp/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from TestApp import forms
-
-# Create your views here.
-def Reg_view(request):
-    frm=forms.Reg_form()
-    return render(request,'testapp/home.html',{'FRM':frm})
