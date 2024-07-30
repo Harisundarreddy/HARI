d30391d03783e4369480218ddc3898e1e873716a
Harisundarreddy <121248428+Harisundarreddy@users.noreply.github.com>
2024-07-30 13:17:46 +0530

    Delete pro1 directory

--- pro1/TestApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from TestApp.models import P
-
-# Register your models here.
-class Padmin(admin.ModelAdmin):
-    list_display=['fname','lname','phoneno','age','J_date','symptoms','prescription']
-
-admin.site.register(P,Padmin)
